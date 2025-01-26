8d005073bfb8a62f14db97d19ca634d7a47dbc2f
dnlklmn <pelicansinberlin@gmail.com>
2025-01-26 21:45:51 +0100

    Add social handles

--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <div
-      className="primary w-full flex flex-col text-center"
+      className="primary w-full flex flex-col text-center font-Franklin"
       style={{
         fontSize: state.fontSize,
         paddingLeft: state.padding * 2,
@@ -23,10 +23,31 @@ export default function Home() {
         className="w-full text-center h-full items-center flex flex-col justify-center"
         style={{ gap: state.padding }}
       >
-        <p className="font-Franklin font-black text-6xl uppercase">DANIEL KALMAN</p>
+        <p className="font-black text-6xl uppercase">DANIEL KALMAN</p>
         <p className="font-Gentium text-2xl">Soon online.</p>
       </div>
-      <div className="h-24" />
+      <div className="h-24">
+        <ul className="flex items-center justify-center gap-8">
+          <li>
+            <a href="https://github.com/dnlklmn" rel="me" className="group">
+              <p>Github</p>
+              <div className="h-[2px] my-1 bg-transparent group-hover:bg-black" />
+            </a>
+          </li>
+          <li>
+            <a href="https://bsky.app/profile/dnlklmn.online" rel="me" className="group">
+              <p>Bluesky</p>
+              <div className="h-[2px] my-1 bg-transparent group-hover:bg-black" />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:dnl.klmn@gmail.com" rel="me" className="group">
+              <p>Email</p>
+              <div className="h-[2px] my-1 bg-transparent group-hover:bg-black" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
